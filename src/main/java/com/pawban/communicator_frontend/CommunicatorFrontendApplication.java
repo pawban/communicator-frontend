affbaef64934b915643910c1d13649758982e210
gawelx <gawelx@gmail.com>
2019-11-17 20:00:24 +0100

    Project initialization

---
+++ src/main/java/com/pawban/communicator_frontend/CommunicatorFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.pawban.communicator_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicatorFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunicatorFrontendApplication.class, args);
+    }
+
+}
