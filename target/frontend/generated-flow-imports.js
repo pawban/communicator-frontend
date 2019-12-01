8f71c625e9c457da6c8222a582d8c930dea179ac
gawelx <gawelx@gmail.com>
2019-12-01 20:19:33 +0100

    Heroku deployment - first try

---
+++ target/frontend/generated-flow-imports.js
@@ -0,0 +1,18 @@
+const div = document.createElement('div');
+div.innerHTML = '<custom-style><style include="lumo-color lumo-typography"></style></custom-style>';
+document.head.insertBefore(div.firstElementChild, document.head.firstChild);
+
+import '@vaadin/vaadin-lumo-styles/color.js';
+import '@vaadin/vaadin-lumo-styles/typography.js';
+import '@vaadin/vaadin-lumo-styles/sizing.js';
+import '@vaadin/vaadin-lumo-styles/spacing.js';
+import '@vaadin/vaadin-lumo-styles/style.js';
+import '@vaadin/vaadin-lumo-styles/icons.js';
+import '@vaadin/flow-frontend/flow-component-renderer.js';
+import '@vaadin/flow-frontend/comboBoxConnector-es6.js';
+import '@vaadin/vaadin-combo-box/theme/lumo/vaadin-combo-box.js';
+import '@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-text-field/theme/lumo/vaadin-text-field.js';
+import '@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-button/theme/lumo/vaadin-button.js';
+import '@vaadin/flow-frontend/comboBoxConnector.js';
