4ac00efe64e9fc545cc1265f71683ad92482d94c
gawelx <gawelx@gmail.com>
2020-01-02 13:19:11 +0100

    Main layout used in both views

--- src/main/java/com/pawban/communicator_frontend/view/MainLayout.java
+++ src/main/java/com/pawban/communicator_frontend/view/MainLayout.java
@@ -1,41 +1,42 @@
 package com.pawban.communicator_frontend.view;
 
-import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.HasElement;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.RouterLayout;
+import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 
 import java.util.Objects;
 
-public class MainLayout extends Composite<Div> implements RouterLayout {
+@Push
+@Theme(value = Lumo.class)
+public class MainLayout extends VerticalLayout implements RouterLayout {
 
-    private final Div content = new Div();
+    private final HorizontalLayout content;
 
     public MainLayout() {
         H2 title = new H2("communicator v0.1");
-        title.setClassName("title");
-        title.getStyle().set("margin", "auto");
+        title.getStyle().set("margin-top", "var(--lumo-space-m)");
         title.setWidthFull();
-        title.setHeight("3vh");
-        title.setMaxHeight("3vh");
-
         VerticalLayout header = new VerticalLayout();
         header.getThemeList().add(Lumo.DARK);
         header.add(title);
-        header.setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
 
-        content.setWidthFull();
-        content.setMaxWidth("100vw");
-        content.setHeight("90vh");
-        content.setMaxHeight("90vh");
-        content.getStyle().set("display", "flex");
-        content.getStyle().set("justify-content", "center");
+        content = new HorizontalLayout();
+        content.setSizeFull();
+        content.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        content.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
 
-        getContent().add(new VerticalLayout(header, content));
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+        add(header, content);
+        setSizeFull();
+        expand(content);
     }
 
     @Override
